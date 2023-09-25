57c7651dc74ef2ac267a1950f795dd067b4716df
Krishna Pande <95022153+KrishnaPande@users.noreply.github.com>
2023-09-25 09:58:51 +0530

    Update models.py

--- erp_with_django/base/models.py
+++ erp_with_django/base/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.contrib.auth import User
 # Create your models here.
 
 class Room(models.Model):
@@ -11,6 +11,17 @@ class Room(models.Model):
     # when ever save is clicked it will go and add a time stamp
     updated = models.DateTimeField(auto_now=True)
     # auto_now will save at every single time and auto_now_add will save only fist instance
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Massage(models.Model):
+    # user =
+    # when room is deleted we need children also be deleted
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    body = models.TextField()
+    updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
