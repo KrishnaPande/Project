4085537b2d6408d5ab4629876150cd84a98968d7
Krishna Pande <95022153+KrishnaPande@users.noreply.github.com>
2023-10-01 23:38:20 +0530

    Added new html file
    CRUD

--- erp_with_django/base/views.py
+++ erp_with_django/base/views.py
@@ -20,5 +20,8 @@ def room(request, pk):
         if i['id'] == int(pk):
             room = i
     context = {'room': room}
-
     return render(request, "base/room.html", context)
+
+def createRoom(request):
+    context = {}
+    return render(request, 'base/room_form.html', context)
